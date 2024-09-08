692230357d1f2679fb0d1a41c0f4420586188e2b
PranithGajula <131193216+PranithGajula@users.noreply.github.com>
2024-09-08 13:37:31 +0530

    Add files via upload

---
+++ Task2_Grade.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+public class Task2_Grade
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter number of subjects:");
+        int sub=scanner.nextInt();
+        int total_marks=0;
+        for(int i=1;i<=sub;i++)
+        {
+            System.out.println("Enter the "+ i + " Subject marks:");
+            total_marks+=scanner.nextInt();
+        }
+        double percentage= (double)total_marks/sub;
+        if(percentage>=90)
+        {
+            System.out.println("Total Marks: "+ total_marks);
+            System.out.println("Average Percentage: "+ percentage);
+            System.out.println("Grade: O");
+        }
+        else if(percentage>=80)
+        {
+            System.out.println("Total Marks: "+ total_marks);
+            System.out.println("Average Percentage: "+ percentage);
+            System.out.println("Grade: A+");
+        }
+        else if(percentage>=70)
+        {
+            System.out.println("Total Marks: "+ total_marks);
+            System.out.println("Average Percentage: "+ percentage);
+            System.out.println("Grade: A");
+        }
+        else if(percentage>=60)
+        {
+            System.out.println("Total Marks: "+ total_marks);
+            System.out.println("Average Percentage: "+ percentage);
+            System.out.println("Grade: B+");
+        }
+        else if(percentage>=50)
+        {
+            System.out.println("Total Marks: "+ total_marks);
+            System.out.println("Average Percentage: "+ percentage);
+            System.out.println("Grade: C");
+        }
+        else if(percentage>=40)
+        {
+            System.out.println("Total Marks: "+ total_marks);
+            System.out.println("Average Percentage: "+ percentage);
+            System.out.println("Grade: D");
+        }
+        else
+        {
+            System.out.println("Total Marks: "+ total_marks);
+            System.out.println("Average Percentage: "+ percentage);
+            System.out.println("Grade: F");
+        }
+        scanner.close();
+    }
+    
+}
---
+++ Task3_ATMInterface.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+class ATM
+{
+    int balance=1000;       //initial account balance is intialized to $1000
+    UserBankAccount obj;
+    public ATM()
+    {
+        obj=new UserBankAccount();
+        obj.setValue(balance);
+        
+    }
+    protected void Withdrawal(int amount)
+    {
+        if(balance<amount)
+        {
+            System.out.println("Insufficient Funds\nPlease Try another amount");
+        }
+        else
+        {
+        balance-=amount;
+        System.out.println("Take your card and wait for the cash");
+        System.out.println("Withdrawal of $"+amount+" successfull");
+        }
+    }
+    protected void deposit(int amount)
+    {
+        balance+=amount;
+        System.out.println("Deposit of $"+amount+" is successfull");
+    }
+    protected void checkBalance()
+    {
+        System.out.println("Your current Bank Balance is $"+balance);
+    }
+
+
+}
+class UserBankAccount
+{
+    private int accountBalance;
+
+    public void setValue(int accountBalance)
+    {
+        this.accountBalance=accountBalance;
+    }
+    public int getAccountBalance()
+    {
+        return accountBalance;
+    }
+}
+
+
+public class Task3_ATMInterface 
+{
+    public static void main(String[] args) 
+    {
+        int choice=0,amount;
+        Scanner sc=new Scanner(System.in);
+        ATM atm =new ATM();
+        System.out.println("Welcome");
+        while(choice!=4)
+        {
+            System.out.println();
+            System.out.println("*********Select the following options*********");
+            System.out.println(" 1.Withdrawal\n 2.Deposit\n 3.Check Balance\n 4.Exit");
+            choice=sc.nextInt();
+            if(choice==1)
+            {
+                System.out.println("Enter the amount you want to withdraw");
+                amount=sc.nextInt();
+                atm.Withdrawal(amount);
+            }
+            else if(choice==2)
+            {
+                System.out.println("Enter the amount to deposit");
+                amount=sc.nextInt();
+                atm.deposit(amount);
+            }
+            else if(choice==3)
+            {
+                atm.checkBalance();
+            }
+            else
+            {
+                System.out.println("ThankYou:)\nVisit Again");
+                choice=4;
+            }
+        }
+        
+        sc.close();
+        
+    }
+    
+}
